db23053dc9681504863f7ee3cef1f0db3ff9d0cc
tsureck <56918122+tsureck@users.noreply.github.com>
2022-05-02 12:56:14 +0200

    Added Footer

---
+++ js/footer.js
@@ -0,0 +1,7 @@
+document.write(`
+    <div class="footer">
+      <footer>
+        <p>Schneverdinger TC e.V. &#8226 Freudenthalstr. 55 &#8226 29640 Schneverdingen &#8226 Telefon Clubhaus 05193 / 4133</p>
+      </footer>
+    </div>
+`);
